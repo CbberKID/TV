3e22eb66ff7a07292704ebf9d6fc84cec7e2a48a
CbberKID <853055381@qq.com>
2025-02-27 12:57:15 +0800

    Update LiveConfig.java
    Signed-off-by: CbberKID <853055381@qq.com>

--- app/src/main/java/com/fongmi/android/tv/api/config/LiveConfig.java
+++ app/src/main/java/com/fongmi/android/tv/api/config/LiveConfig.java
@@ -73,7 +73,7 @@ public class LiveConfig {
     }
 
     public static boolean hasUrl() {
-        return getUrl() != null && getUrl().length() > 0;
+        return true; // getUrl() != null && getUrl().length() > 0;
     }
 
     public static void load(Config config, Callback callback) {
