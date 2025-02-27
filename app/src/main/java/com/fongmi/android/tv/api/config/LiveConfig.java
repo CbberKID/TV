ac82f6f6964ff1f8f7ea9ff1210b6772337a573a
CbberKID <853055381@qq.com>
2025-02-27 13:24:04 +0800

    Update LiveConfig.java
    Signed-off-by: CbberKID <853055381@qq.com>

--- app/src/main/java/com/fongmi/android/tv/api/config/LiveConfig.java
+++ app/src/main/java/com/fongmi/android/tv/api/config/LiveConfig.java
@@ -115,9 +115,8 @@ public class LiveConfig {
         try {
             String url = config.getUrl();
             if (TextUtils.isEmpty(url)) {
-                url = &quot;http://101.43.3.89/kid.json&quot;;
-              // 添加以下代码，解决内置源时，投屏播放问题，给定一个配置，写入本地数据库，标记一个name（名字“源已内置”可以随便取，但一定要有，type为1,表示直播）
-                Config.find(url, 1).name(&quot;内置站源&quot;).update();
+                url = "http://101.43.3.89/kid.json";
+                Config.find(url, 1).name("内置站源").update();
             }
             parseConfig(Decoder.getJson(url), callback);
             // parseConfig(Decoder.getJson(config.getUrl()), callback);
