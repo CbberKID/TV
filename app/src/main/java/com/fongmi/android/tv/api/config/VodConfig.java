3387f368b580eb34ff427d640f4bb41ec6e61fda
CbberKID <853055381@qq.com>
2025-02-27 13:24:53 +0800

    Update VodConfig.java
    Signed-off-by: CbberKID <853055381@qq.com>

--- app/src/main/java/com/fongmi/android/tv/api/config/VodConfig.java
+++ app/src/main/java/com/fongmi/android/tv/api/config/VodConfig.java
@@ -113,9 +113,8 @@ public class VodConfig {
         try {
             String url = config.getUrl();
             if (TextUtils.isEmpty(url)) {
-                url = &quot;http://101.43.3.89/kid.json&quot;;
-                // 添加以下代码，解决内置源时，投屏播放问题，给定一个配置，写入本地数据库，标记一个name（名字“源已内置”可以随便取，但一定要有，type为0,表示点播）
-                Config.find(url, 0).name(&quot;内置站源&quot;).update();
+                url = "http://101.43.3.89/kid.json";
+                Config.find(url, 0).name("内置站源").update();
             }
             checkJson(JsonParser.parseString(Decoder.getJson(url)).getAsJsonObject(), callback);
             // checkJson(Json.parse(Decoder.getJson(config.getUrl())).getAsJsonObject(), callback);
