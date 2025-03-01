e12d801764011ff2881f0258962c5b7285ee7664
CbberKID <853055381@qq.com>
2025-03-01 19:09:11 +0800

    Update SettingActivity.java
    Signed-off-by: CbberKID <853055381@qq.com>

--- app/src/leanback/java/com/fongmi/android/tv/ui/activity/SettingActivity.java
+++ app/src/leanback/java/com/fongmi/android/tv/ui/activity/SettingActivity.java
@@ -276,11 +276,11 @@ public class SettingActivity extends BaseActivity implements ConfigCallback, Sit
     }
 
     private void onVersion(View view) {
-        Updater.get().force().release().start(this);
+        Updater.get().force().start(this);
     }
 
     private boolean onVersionDev(View view) {
-        Updater.get().force().dev().start(this);
+        Updater.get().force().start(this);
         return true;
     }
 
