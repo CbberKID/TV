204ba054885fb1595cafb13444f0d57037420f75
CbberKID <853055381@qq.com>
2025-03-01 20:07:19 +0800

    Update Github.java
    Signed-off-by: CbberKID <853055381@qq.com>

--- catvod/src/main/java/com/github/catvod/utils/Github.java
+++ catvod/src/main/java/com/github/catvod/utils/Github.java
@@ -3,19 +3,18 @@ package com.github.catvod.utils;
 public class Github {
 
     // 修改为你的服务器基础地址
-    private static final String URL = "http://www.176868.xyz/fongmi";
+    public static final String URL = "http://www.176868.xyz/fongmi";
 
-    private static String getUrl(boolean dev, String name) {
-        // dev模式使用独立路径（如dev目录），非dev使用release目录
-        String path = dev ? "dev" : "release";
-        return URL + "/" + path + "/" + name;
+    private static String getUrl(boolean dev, String type, String name) {
+        String path = dev ? "dev" : "release"; // 开发版/正式版路径
+        return URL + "/" + type + "/" + path + "/" + name; // 按设备类型分目录
     }
 
-    public static String getJson(boolean dev, String name) {
-        return getUrl(dev, name + ".json"); // 生成JSON路径
+    public static String getJson(boolean dev, String type) {
+        return getUrl(dev, type, "update.json");
     }
 
     public static String getApk(boolean dev, String name) {
-        return getUrl(dev, name + ".apk"); // 生成APK路径
+        return getUrl(dev, name.split("-")[0], name + ".apk"); // 从name中提取类型（tv/mobile）
     }
 }
